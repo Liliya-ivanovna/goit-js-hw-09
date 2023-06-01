bdb1494fa00b456f57aba98ea49fab57c1e281d1
Liliya-ivanovna <123566371+Liliya-ivanovna@users.noreply.github.com>
2023-06-01 08:10:05 +0000

    Deploying to gh-pages from @ Liliya-ivanovna/goit-js-hw-09@a0b12d7af08fa3701bb98c035c2b8ff56a4a1242 🚀

---
+++ 01-color-switcher.263e6a2b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");t&&"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}();
+//# sourceMappingURL=01-color-switcher.263e6a2b.js.map
