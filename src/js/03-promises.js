592495411ff64cc265af560b39fc021dbdb6a14e
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-06-03 20:56:24 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,21 @@
 import Notiflix from 'notiflix';
 const formEl = document.getElementsByClassName('form');
-formEl.addEventListener('submit', createPromise);
+const amount =document.getElementsByName("amount");
+const step =document.getElementsByName("step");
 function createPromise(position, delay) {
 const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    
+    return (delay+step)
   } else {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     // Reject
   }
 }
-createPromise(2, 1500)
-.then(({ position, delay }) => {
-  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-})
-.catch(({ position, delay }) => {
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-});
+createPromise(2,1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
